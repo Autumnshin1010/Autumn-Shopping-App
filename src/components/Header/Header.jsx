3651c37e601f485e1507e4ff1c82efebb129d37f
Autumn <autumn.1010.shin@gmail.com>
2024-06-05 12:09:05 -0700

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order your <br />
+          favorite food here</h2>
+        <p>Small bites, big flavor. Order today and get free apps for life.</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
